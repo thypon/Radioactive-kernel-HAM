09ddbb326901fb2d42176912762da31ff249acb1
andip71 <andreasp@gmx.de>
2014-11-12 22:51:58 +0100

    cpufreq: add CONFIG_MSM_CPU_FREQ_SET_MIN_MAX functionality back to backported MSM 3.10 devfreq
    credits to anarkia1976
    
    Conflicts:
    	arch/arm/mach-msm/cpufreq.c

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -270,16 +270,19 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 		policy->cpuinfo.min_freq = CONFIG_MSM_CPU_FREQ_MIN;
 		policy->cpuinfo.max_freq = CONFIG_MSM_CPU_FREQ_MAX;
 #endif
+		pr_err("cpufreq: failed to get policy min/max\n");
 	}
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
 	policy->min = CONFIG_MSM_CPU_FREQ_MIN;
 	policy->max = CONFIG_MSM_CPU_FREQ_MAX;
+#else
+#ifdef CONFIG_ARCH_MSM8974
+	/* Predefine max/min frequencies used for device boot */
+	policy->max = 2457600;
+	policy->min = 300000;
 #endif
-
-	if (is_clk)
-		cur_freq = clk_get_rate(cpu_clk[policy->cpu])/1000;
-	else
-		cur_freq = acpuclk_get_rate(policy->cpu);
+#endif
+	cur_freq = clk_get_rate(cpu_clk[policy->cpu])/1000;
 
 	if (cpufreq_frequency_table_target(policy, table, cur_freq,
 	    CPUFREQ_RELATION_H, &index) &&
@@ -296,13 +299,21 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	ret = set_cpu_freq(policy, table[index].frequency, table[index].index);
 	if (ret)
 		return ret;
+	/* Use user max frequency instead of max available frequency */
 	pr_debug("cpufreq: cpu%d init at %d switching to %d\n",
+#ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
+			policy->cpu, cur_freq, policy->max);
+	policy->cur = policy->max;
+#else
 			policy->cpu, cur_freq, table[index].frequency);
 	policy->cur = table[index].frequency;
-
-	policy->cpuinfo.transition_latency =
-		acpuclk_get_switch_time() * NSEC_PER_USEC;
-
+#endif
+	cpufreq_frequency_table_get_attr(table, policy->cpu);
+#ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
+	/* set safe default min and max speeds */
+	policy->max = CONFIG_MSM_CPU_FREQ_MAX;
+	policy->min = CONFIG_MSM_CPU_FREQ_MIN;
+#endif
 	return 0;
 }
 
