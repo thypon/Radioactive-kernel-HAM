583c489e55aaa49007afeab28d940566a644532f
Evisceration <eviscerationls@gmail.com>
2014-12-29 22:47:56 +0100

    msm: mdss: do not call panel off multiple times
    expand the check so panel off does not get called multiple times
    
    Change-Id: Idec082524e0c2060cedd43c3536e0072608796da
    Signed-off-by: Evisceration <eviscerationls@gmail.com>
    
    Conflicts:
    	drivers/video/msm/mdss/dsi_v2.c

--- drivers/video/msm/mdss/dsi_v2.c
+++ drivers/video/msm/mdss/dsi_v2.c
@@ -94,25 +94,15 @@ static int dsi_panel_handler(struct mdss_panel_data *pdata, int enable)
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
-	if (enable) {
-		if (!pdata->panel_info.dynamic_switch_pending) {
-			if (pdata->panel_info.type == MIPI_CMD_PANEL)
-				dsi_ctrl_gpio_request(ctrl_pdata);
-			mdss_dsi_panel_reset(pdata, 1);
-		}
-		pdata->panel_info.panel_power_on = 1;
-		if (!pdata->panel_info.dynamic_switch_pending) {
-			rc = ctrl_pdata->on(pdata);
-			if (rc)
-				pr_err("%s: panel on failed!\n", __func__);
-		}
-		if (pdata->panel_info.type == MIPI_CMD_PANEL &&
-				pdata->panel_info.dynamic_switch_pending) {
-			dsi_ctrl_gpio_request(ctrl_pdata);
-			mdss_dsi_set_tear_on(ctrl_pdata);
-		}
-	} else {
-		msm_dsi_sw_reset();
+	if (enable &&
+		(pdata->panel_info.panel_power_state == MDSS_PANEL_POWER_OFF)) {
+		mdss_dsi_panel_reset(pdata, 1);
+		pdata->panel_info.panel_power_state = MDSS_PANEL_POWER_ON;
+		rc = ctrl_pdata->on(pdata);
+		if (rc)
+			pr_err("dsi_panel_handler panel on failed %d\n", rc);
+	} else if (!enable &&
+		(pdata->panel_info.panel_power_state == MDSS_PANEL_POWER_ON)) {
 		if (dsi_intf.op_mode_config)
 			dsi_intf.op_mode_config(DSI_CMD_MODE, pdata);
 		if (pdata->panel_info.dynamic_switch_pending) {
