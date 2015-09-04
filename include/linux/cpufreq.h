e47fdb57ccf28a5acaefbeac51174451e1ec7b38
acuicultor <ramoncuenca4@gmail.com>
2015-09-04 20:41:43 +0200

    Introduce barry allen governor and activate adaptive
    Conflicts:
    	arch/arm/configs/radioactive_defconfig
    	drivers/cpufreq/Kconfig
    	radioactive-bacon-build.sh

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -391,6 +391,12 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
 extern struct cpufreq_governor cpufreq_gov_adaptive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_adaptive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
+extern struct cpufreq_governor cpufreq_gov_barry_allen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
 #endif
 
 
