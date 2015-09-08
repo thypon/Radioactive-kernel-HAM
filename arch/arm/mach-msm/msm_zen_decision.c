68b6b034d6b68abf6a7490638fc4440d67c55bc8
acuicultor <ramoncuenca4@gmail.com>
2015-09-08 17:45:32 +0200

    update to zen decision v2.0

--- arch/arm/mach-msm/msm_zen_decision.c
+++ arch/arm/mach-msm/msm_zen_decision.c
@@ -25,17 +25,21 @@
 #include <linux/platform_device.h>
 #include <linux/workqueue.h>
 #include <linux/power_supply.h>
+#include <linux/mutex.h>
 
 #define ZEN_DECISION "zen_decision"
 
-/* Enable/Disable driver */
-unsigned int enabled = 0;
+/*
+ * Enable/Disable driver
+ */
+unsigned int enabled = 1;
 
 /*
  * How long to wait to enable cores on wake (in ms)
  */
 #define WAKE_WAIT_TIME_MAX 60000 // 1 minute maximum
 unsigned int wake_wait_time = 1000;
+>>>>>>> 3662838... msm: zen_decision v2.0
 
 /*
  * Battery level threshold to ignore UP operations.
@@ -56,16 +60,10 @@ static struct delayed_work wake_work;
 struct kobject *zendecision_kobj;
 
 /* Power supply information */
+// If there is no power_supply device named "battery", battery calculation will be ignored.
+char ps_name[] = "battery";
 static struct power_supply *psy;
 union power_supply_propval current_charge;
-
-/*
- * Some devices may have a different name power_supply device representing the battery.
- *
- * If we can't find the "battery" device, then we ignore all battery work, which means
- * we do the CPU_UP work regardless of the battery level.
- */
-char ps_name[] = "battery";
 
 static int get_power_supply_level(void)
 {
@@ -75,12 +73,6 @@ static int get_power_supply_level(void)
 		return ret;
 	}
 
-	/*
-	 * On at least some MSM devices POWER_SUPPLY_PROP_CAPACITY represents current
-	 * battery level as an integer between 0 and 100.
-	 *
-	 * Unknown if other devices use this property or a different one.
-	 */
 	ret = psy->get_property(psy, POWER_SUPPLY_PROP_CAPACITY, &current_charge);
 	if (ret)
 		return ret;
@@ -89,30 +81,37 @@ static int get_power_supply_level(void)
 }
 
 /*
- * msm_zd_online_cpus
+ * __msm_zen_dec_wake
  *
  * Core wake work function.
  * Brings all CPUs online. Called from worker thread.
  */
-static void __ref msm_zd_online_all_cpus(struct work_struct *work)
+static void __ref __msm_zen_dec_wake(struct work_struct *work)
 {
 	int cpu;
 
 	for_each_cpu_not(cpu, cpu_online_mask) {
+		/* Don't call cpu_up if cpu0 */
+		if (cpu == 0) continue;
 		cpu_up(cpu);
 	}
 }
 
 /*
- * msm_zd_queue_online_work
+ * msm_zen_dec_wake
  *
- * Call msm_zd_online_all_cpus as a delayed worker thread on wake_wq.
+ * Call __msm_zen_dec_wake as a delayed worker thread on wake_wq.
  * Delayed by wake_wait_time.
  */
-static void msm_zd_queue_online_work(void)
+static int msm_zen_dec_wake(void)
 {
-	queue_delayed_work(zen_wake_wq, &wake_work,
+	int ret;
+
+	INIT_DELAYED_WORK(&wake_work, __msm_zen_dec_wake);
+	ret = queue_delayed_work_on(0, zen_wake_wq, &wake_work,
 			msecs_to_jiffies(wake_wait_time));
+
+	return ret;
 }
 
 /** Use FB notifiers to detect screen off/on and do the work **/
@@ -126,20 +125,20 @@ static int fb_notifier_callback(struct notifier_block *nb,
 	if (!enabled)
 		return 0;
 
-	/* Clear wake workqueue of any pending threads */
+	/* Clear wake workqueue */
 	flush_workqueue(zen_wake_wq);
 	cancel_delayed_work_sync(&wake_work);
 
 	if (evdata && evdata->data && event == FB_EVENT_BLANK) {
 		blank = evdata->data;
 		if (*blank == FB_BLANK_UNBLANK) {
-			/* Always queue work if PS device doesn't exist or bat_threshold_ignore == 0 */
-			if (psy && bat_threshold_ignore) {
-				/* If current level > ignore threshold, then queue UP work */
+			// Make decision based on bat_threshold_ignore
+			if (psy && bat_threshold_ignore)
+				// If current level > ignore threshold, then queue UP work
 				if (get_power_supply_level() > bat_threshold_ignore)
-					msm_zd_queue_online_work();
-			} else
-				msm_zd_queue_online_work();
+					msm_zen_dec_wake();
+			else
+				msm_zen_dec_wake();
 		}
 	}
 
@@ -210,8 +209,8 @@ static ssize_t bat_threshold_ignore_store(struct kobject *kobj,
 	/* Restrict between 0 and 100 */
 	if (new_val > 100)
 		bat_threshold_ignore = 100;
-	else
-		bat_threshold_ignore = new_val;
+	else if (new_val < 0)
+		bat_threshold_ignore = 0;
 
 	return size;
 }
@@ -228,22 +227,25 @@ static struct kobj_attribute kobj_bat_threshold_ignore =
 	__ATTR(bat_threshold_ignore, 0644, bat_threshold_ignore_show,
 		bat_threshold_ignore_store);
 
-static struct attribute *zd_attrs[] = {
+static struct attribute *zen_decision_attrs[] = {
 	&kobj_enabled.attr,
 	&kobj_wake_wait.attr,
 	&kobj_bat_threshold_ignore.attr,
 	NULL,
 };
 
-static struct attribute_group zd_option_group = {
-	.attrs = zd_attrs,
+static struct attribute_group zen_decision_option_group = {
+	.attrs = zen_decision_attrs,
 };
 
 /* Sysfs End */
 
-static int zd_probe(struct platform_device *pdev)
+static int zen_decision_probe(struct platform_device *pdev)
 {
 	int ret;
+
+	/* Set default settings */
+	enabled = 1;
 
 	/* Setup sysfs */
 	zendecision_kobj = kobject_create_and_add("zen_decision", kernel_kobj);
@@ -252,19 +254,18 @@ static int zd_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
-	ret = sysfs_create_group(zendecision_kobj, &zd_option_group);
+	ret = sysfs_create_group(zendecision_kobj, &zen_decision_option_group);
 	if (ret) {
 		pr_info("[%s]: sysfs interface failed to initialize\n", ZEN_DECISION);
 		return -EINVAL;
 	}
 
 	/* Setup Workqueues */
-	zen_wake_wq = alloc_workqueue("zen_wake_wq", WQ_FREEZABLE | WQ_UNBOUND, 1);
+	zen_wake_wq = alloc_workqueue("zen_wake_wq", WQ_FREEZABLE, 0);
 	if (!zen_wake_wq) {
 		pr_err("[%s]: Failed to allocate suspend workqueue\n", ZEN_DECISION);
 		return -ENOMEM;
 	}
-	INIT_DELAYED_WORK(&wake_work, msm_zd_online_all_cpus);
 
 	/* Setup FB Notifier */
 	fb_notifier.notifier_call = fb_notifier_callback;
@@ -287,7 +288,7 @@ static int zd_probe(struct platform_device *pdev)
 	return ret;
 }
 
-static int zd_remove(struct platform_device *pdev)
+static int zen_decision_remove(struct platform_device *pdev)
 {
 	kobject_put(zendecision_kobj);
 
@@ -301,30 +302,30 @@ static int zd_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static struct platform_driver zd_driver = {
-	.probe = zd_probe,
-	.remove = zd_remove,
+static struct platform_driver zen_decision_driver = {
+	.probe = zen_decision_probe,
+	.remove = zen_decision_remove,
 	.driver = {
 		.name = ZEN_DECISION,
 		.owner = THIS_MODULE,
 	}
 };
 
-static struct platform_device zd_device = {
+static struct platform_device zen_decision_device = {
 	.name = ZEN_DECISION,
 	.id = -1
 };
 
-static int __init zd_init(void)
+static int __init zen_decision_init(void)
 {
-	int ret = platform_driver_register(&zd_driver);
+	int ret = platform_driver_register(&zen_decision_driver);
 	if (ret)
 		pr_err("[%s]: platform_driver_register failed: %d\n", ZEN_DECISION, ret);
 	else
 		pr_info("[%s]: platform_driver_register succeeded\n", ZEN_DECISION);
 
 
-	ret = platform_device_register(&zd_device);
+	ret = platform_device_register(&zen_decision_device);
 	if (ret)
 		pr_err("[%s]: platform_device_register failed: %d\n", ZEN_DECISION, ret);
 	else
@@ -333,17 +334,16 @@ static int __init zd_init(void)
 	return ret;
 }
 
-static void __exit zd_exit(void)
+static void __exit zen_decision_exit(void)
 {
-	platform_driver_unregister(&zd_driver);
-	platform_device_unregister(&zd_device);
+	platform_driver_unregister(&zen_decision_driver);
+	platform_device_unregister(&zen_decision_device);
 }
 
-late_initcall(zd_init);
-module_exit(zd_exit);
+late_initcall(zen_decision_init);
+module_exit(zen_decision_exit);
 
 MODULE_VERSION("2.0");
 MODULE_DESCRIPTION("Zen Decision - Kernel MSM Userspace Handler");
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Brandon Berhent <bbedward@gmail.com>");
-
