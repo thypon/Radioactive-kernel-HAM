7f5b7cdc73c2d5ef153a28e2723ebe571528eae9
acuicultor <ramoncuenca4@gmail.com>
2016-04-22 17:50:06 +0200

    net: replace percpu_xxx funcs with this_cpu_xxx or __this_cpu_xxx
    percpu_xxx funcs are duplicated with this_cpu_xxx funcs, so replace
    them for further code clean up.
    
    And in preempt safe scenario, __this_cpu_xxx funcs may has a bit
    better performance since __this_cpu_xxx has no redundant
    preempt_enable/preempt_disable on some architectures.
    
    Signed-off-by: Alex Shi <alex.shi@intel.com>
    Acked-by: Eric Dumazet <eric.dumazet@gmail.com>
    Acked-by: David S. Miller <davem@davemloft.net>
    Cc: Patrick McHardy <kaber@trash.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    
    Conflicts:
    	net/socket.c (reverted from commit 0e091df6ae6f6d0c2999b0d6c8088517a0cf045c)

--- net/socket.c
+++ net/socket.c
@@ -489,7 +489,7 @@ static struct socket *sock_alloc(void)
 	inode->i_uid = current_fsuid();
 	inode->i_gid = current_fsgid();
 
-	this_cpu_add(sockets_in_use, 1);
+	percpu_add(sockets_in_use, 1);
 	return sock;
 }
 
@@ -532,10 +532,7 @@ void sock_release(struct socket *sock)
 	if (rcu_dereference_protected(sock->wq, 1)->fasync_list)
 		printk(KERN_ERR "sock_release: fasync list not empty!\n");
 
-	if (test_bit(SOCK_EXTERNALLY_ALLOCATED, &sock->flags))
-		return;
-
-	this_cpu_sub(sockets_in_use, 1);
+	percpu_sub(sockets_in_use, 1);
 	if (!sock->file) {
 		iput(SOCK_INODE(sock));
 		return;
