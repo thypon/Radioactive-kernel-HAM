e6035e40ce6903e3705487d106fbd32e3b8a00ad
acuicultor <ramoncuenca4@gmail.com>
2016-05-20 09:59:23 +0200

    Reborn is here!!!! Thanks to JoaoPaulo for his patches and cool added features
    Update build version and change kernel name also switch again to linaro 4.8

--- radioactive-bacon-build.sh
+++ radioactive-bacon-build.sh
@@ -16,12 +16,12 @@ DEFCONFIG="radioactive_defconfig"
 
 # Kernel Details
 BASE_RADIOACTIVE_VER="RADIOACTIVE_"
-VER="KERNEL_V1.3"
+VER="REBORN_V2.0"
 RADIOACTIVE_VER="$BASE_RADIOACTIVE_VER$VER"
 
 # Vars
 export LOCALVERSION=~`echo $RADIOACTIVE_VER`
-export CROSS_COMPILE=${HOME}/Android/toolchains/ubertc-5.3/bin/arm-eabi-
+export CROSS_COMPILE=${HOME}/Android/toolchains/linaro4.8/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=${USER}
